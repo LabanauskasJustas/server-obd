46d403a659a1ddbcd4c723e77961c3ec4e4a5c67
Justas <labanauskas.justas@gmail.com>
2020-12-06 19:56:40 +0200

    Removed bs files

--- index.php
+++ index.php
@@ -1,44 +1,29 @@
 <?php
-include("include/session.php");
-include("api.php");
+include ("api.php");
 ?>
-<html>
+<!DOCTYPE html>
+<html lang="">
 <head>
-    <meta http-equiv="X-UA-Compatible" content="IE=9; text/html; charset=utf-8"/>
-    <title>Demo projektas</title>
-    <link href="include/styles.css" rel="stylesheet" type="text/css" />
+    <title>LOGIN</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-<table class="center" ><tr><td>
+<form action="login.php" method="post">
+    <h2>LOGIN</h2>
+    <?php if (isset($_GET['error'])) { ?>
+        <p class="error"><?php echo $_GET['error']; ?></p>
+    <?php } ?>
+    <label>User Name</label>
+    <label>
+        <input type="text" name="uname" placeholder="User Name">
+    </label><br>
 
-        </td></tr><tr><td>
-            <?php
-            //Jei vartotojas prisijungęs
-            global $session;global $form;
-            if ($session->logged_in) {
-                include("include/meniu.php");
-                ?>
-                <div style="text-align: center;color:green">
-                    <br><br>
-                    <h1>Pradinis sistemos puslapis (index.php).</h1>
-                </div><br>
-                <?php
-                //Jei vartotojas neprisijungęs, rodoma prisijungimo forma
-                //Jei atsiranda klaidų, rodomi pranešimai.
-            } else {
-                echo "<div align=\"center\">";
-                if ($form->num_errors > 0) {
-                    echo "<font size=\"3\" color=\"#ff0000\">Klaidų: " . $form->num_errors . "</font>";
-                }
-                echo "<table class=\"center\"><tr><td>";
-                include("include/loginForm.php");
-                echo "</td></tr></table></div><br></td></tr>";
-            }
-            echo "<tr><td>";
-            include("include/footer.php");
-            echo "</td></tr>";
-            ?>
-        </td></tr>
-</table>
+    <label>User Name</label>
+    <label>
+        <input type="password" name="password" placeholder="Password">
+    </label><br>
+
+    <button type="submit">Login</button>
+</form>
 </body>
 </html>
